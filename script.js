123f755d56aeec38072962e48ab8f3fb181df796
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-18 15:17:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
+	if(str === null){
+		return[];
+	}
   // your code here
+	let chunk = [];
+	for(let i = 0; i < str.length; i += size){
+		chunk.push(str.slice(i,i+size));
+	}
+	return chunk;
 }
 
 // Do not change the code below
